b8777cfacc8cbbc0260dc9715a210e82056cd587
BBAER <yeybreya@gmail.com>
2025-12-17 18:20:31 +0700

    FN

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,12 +4,25 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double grade){
+    if(grade > 90.0)
+        return  'A';
+    else if(grade > 75.0 && grade <= 90.0)
+        return  'B';
+    else if(grade > 60.0 && grade <= 75.0)
+        return  'C';
+    else if(grade > 45.0 && grade <= 60.0)
+        return  'D';
+    else
+        return  'F';
+}
 
 int main(){
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
+	cin.ignore();
 	string name[N];
 	float score[N];	
 	
@@ -18,8 +31,11 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
+		cin.ignore();
 		i++;
 	}
 	
--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int age;
+    int height;
+    int price;
+    
+    string crt;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    if(age > 25){
+        if(age > 60){
+            crt = "Brook";
+        }else{
+            cout << "Enter your bounty: ";
+            cin >> price;
+            if(price > 500000000){
+                crt = "Jinbe";
+            }else{
+                crt = "Franky";
+            }
+        }
+    }else{
+        cout << "Enter your height: ";
+        cin >> height;
+        
+        if(height < 100){
+            crt = "Chopper";
+        }else{
+            if(height < 180){
+                crt = "Usopp";
+            }else{
+                cout << "Enter your bounty: ";
+                cin >> price;
+                
+                if(price > 1100000000){
+                    crt = "Zoro";
+                }else{
+                    crt = "Sanji";
+                }
+            }
+        }
+    }
+    
+    cout << "Your character = " << crt << endl;
+    return 0;
+}
 /*
 "Enter your age: "
 "Enter your height: "
